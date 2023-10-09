d728115419127bba09fbc07f5ee0d29b13c6abad
cesare-spinoso <cesare.spinoso@gmail.com>
2023-10-09 16:40:07 -0400

    added more

--- docs/assets/utils.js
+++ docs/assets/utils.js
@@ -6,3 +6,42 @@ function HideShowElement(eltId) {
         x.style.display = "none";
     }
 }
+
+// https://jennieablog.github.io/2020/06/20/coding-a-theme-switch.html
+
+function changeTheme() {
+    const theme_switch = document.getElementById("theme-switch");
+    if (theme_switch.checked) {
+        DarkReader.enable();
+    }
+    else {
+        DarkReader.disable();
+    }
+}
+
+function changeTheme() {
+    const theme_switch = document.getElementById("theme-switch");
+    if (theme_switch.checked) {
+        DarkReader.enable();
+        // Add the ff. line to write to memory.
+        localStorage.setItem("my-theme", "dark");
+    }
+    else {
+        DarkReader.disable();
+        // Add the ff. line to write to memory.
+        localStorage.setItem("my-theme", null);
+    }
+}
+
+// Check local storage every reload to know which theme to use.
+const theme_switch = document.getElementById("theme-switch");
+if (localStorage.getItem("my-theme") === "dark") {
+    // Use dark theme.
+    DarkReader.enable();
+    theme_switch.checked = true;
+}
+else {
+    // Use default theme.
+    DarkReader.disable();
+    theme_switch.checked = false;
+}
